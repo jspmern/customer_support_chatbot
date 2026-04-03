36831f83696a895e623f44b8f4b9a6afab64b4e7
jspmern <jspmern@gmail.com>
2026-04-03 10:40:55 +0530

    learning tool with readme

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@
  import { ToolNode } from "@langchain/langgraph/prebuilt"
  import { StateGraph, Annotation, END } from "@langchain/langgraph";
 import { StateAnnotation } from "./state/state.ts";
-import { CATEGORIZATION_HUMAN_PROMPT, CATEGORIZATION_SYSTEM_PROMPT, marketingSystemPrompt, SYSTEM_PROMPT } from "./prompt/prompt.ts";
+import { CATEGORIZATION_HUMAN_PROMPT, CATEGORIZATION_SYSTEM_PROMPT, learningSystemPrompt, marketingSystemPrompt, SYSTEM_PROMPT } from "./prompt/prompt.ts";
 import { getOffer } from "./tools/marketingTools.ts";
 import { AIMessage } from "@langchain/core/messages";
+import { retrieve_lerning_tool } from "./tools/learningTools.ts";
 
 //marketing tools
 const marketingTool=[getOffer]
 const marketingToolNode=new ToolNode(marketingTool)
+
+//learning tools
+const learningTool=[retrieve_lerning_tool]
+const learningToolNode=new ToolNode(learningTool)
 
 async function frontendDeskAgent(state:typeof StateAnnotation.State)
 {
@@ -67,8 +72,28 @@ const marketingResponse=await llmInvoke.invoke([
 }
 async function learningAgent(state:typeof StateAnnotation.State)
 {
-    console.log("hi i am learning agent")
-    console.log('state.messages',state)
+   
+ const llmInvoke=  modal.bindTools(learningTool)
+ 
+ let trimmedHistory = state.messages;
+ 
+    if (trimmedHistory.at(-1)?.getType() === 'ai') {
+        trimmedHistory = trimmedHistory.slice(0, -1); // [1, 2, 3] -> [1, 2]
+    }
+  
+ const learningResponse=await llmInvoke.invoke([
+    {
+        role:"system",
+        content:learningSystemPrompt
+    },
+    ...trimmedHistory
+])
+//console.log("hii i am learningAgentResponse",learningResponse)
+    return {
+        messages:[learningResponse]
+    }
+
+
     return state
 }
 async function whereToGoNext(state:typeof StateAnnotation.State){
@@ -98,11 +123,23 @@ async function whereToGoNextInMarketing(state:typeof StateAnnotation.State)
        }
         return "__end__"     
 }
+
+async function whereToGoNextInLearning(state:typeof StateAnnotation.State)
+{
+     //console.log("******************",state)
+     const lastMessage = state.messages[state.messages.length - 1] as AIMessage;
+       if(lastMessage.tool_calls?.length)
+       {
+        return "learningToolNode"
+       }
+        return "__end__" 
+}
  const graph=new StateGraph(StateAnnotation)
  .addNode("frontendDesk",frontendDeskAgent)
  .addNode("marketing",marketingAgent)
  .addNode("learning",learningAgent)
  .addNode('marketingToolNode',marketingToolNode)
+ .addNode('learningToolNode',learningToolNode)
  .addEdge("__start__","frontendDesk")
   .addConditionalEdges("frontendDesk",whereToGoNext,{
     "marketing":"marketing",
@@ -113,9 +150,13 @@ async function whereToGoNextInMarketing(state:typeof StateAnnotation.State)
    marketingToolNode:"marketingToolNode",
    __end__:END
  })
+ .addConditionalEdges("learning",whereToGoNextInLearning,{
+   learningToolNode:"learningToolNode",
+   __end__:END
+ })
  .addEdge("marketingToolNode","marketing")
  //Todo :--- later we have to change as per the requirment  
- .addEdge("learning","__end__")
+ .addEdge("learningToolNode","learning")
 
  const app=graph.compile()
  async function main(){
@@ -123,7 +164,7 @@ async function whereToGoNextInMarketing(state:typeof StateAnnotation.State)
     messages:[
         {
             role:"user",
-            content:"do i have any offer in nodejs project"
+            content:"do we have course on rust"
         }
     ]
  })
